a7d7010a5f2a963847084de685ef52b27adc954f
Yudi454 <lucasyudi445@gmail.com>
2025-04-23 11:26:07 -0300

    Le di forma al footer, falta agregar el contenido

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,32 @@
+import { Col, Row } from "react-bootstrap";
+
 export const Footer = () => {
   return (
     <>
       <hr />
       <div className="Footer text-center">
-        <h3>Footer</h3>
+        <Row>
+          <Col lg={5}></Col>
+          <Col lg={3}>
+            <Row>
+              <Col lg={6}>Inicio</Col>
+              <Col lg={6}>Sobre mí</Col>
+              <Col lg={6}>Proyectos</Col>
+              <Col lg={6}>Contacto</Col>
+            </Row>
+          </Col>
+          <Col lg={3}>
+            <Row>
+              <Col lg={6}>
+                <i class="Github devicon-github-original-wordmark colored"></i>
+              </Col>
+              <Col lg={6}>
+                <i class="LinkedIn devicon-linkedin-plain-wordmark colored"></i>
+              </Col>
+              <Col></Col>
+            </Row>
+          </Col>
+        </Row>
       </div>
     </>
   );
