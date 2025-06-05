95d9959a03a284194f433f47b202fad82c58227b
Yudi454 <lucasyudi445@gmail.com>
2025-06-05 14:39:15 -0300

    Se agrego todos los componentes al Inicio

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import MainHome from "./MainHome"
+
+export const Home = () => {
+
+
+    return (
+        <>
+        <MainHome />
+        </>
+    )
+}
---
+++ src/pages/home/MainHome.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import AboutMe from '../About_Me/AboutMe'
+import Projects from '../projects/Projects'
+import Contact from '../contact/Contact'
+
+const MainHome = () => {
+  return (
+    <div>
+      <AboutMe/>
+      <Projects/>
+      <Contact/>
+    </div>
+  )
+}
+
+export default MainHome
