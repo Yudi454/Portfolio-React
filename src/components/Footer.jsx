abf425f8908e2e5dbe9930988b1c6e03d616090d
Yudi454 <lucasyudi445@gmail.com>
2025-06-06 00:49:57 -0300

    Se agrego diseño al footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,24 @@
 import { Col, Row } from "react-bootstrap";
 import { useStore } from "../store/AuthStore";
+import "../css/footer/Footer.css"
 
 export const Footer = () => {
   const { redes } = JSON.parse(localStorage.getItem("datos"));
 
-    const color = useStore((state) => state.color);
-  
-  
+  const color = useStore((state) => state.color);
+
   return (
     <>
       <hr />
-      <div className={`fondo${color} text-center fw-bold`}>
+      <div className={`fondo${color} text-center fw-bold pb-4`}>
         <Row className="align-items-center g-0">
-          <Col lg={6}>
-            <a href={redes[0].link} target="_blank">
-              <i className={redes[0].logo}></i>
+          {redes && redes.map((red) => 
+          <Col lg={6} className="pt-4">
+            <a href={red.link} target="_blank">
+              <i className={`${red.logo} icono`}></i>
             </a>
           </Col>
-          <Col lg={6}>
-            <a
-              href={redes[1].link}
-              target="_blank"
-              className="text-decoration-none"
-            >
-              <i className={redes[1].logo}></i>
-            </a>
-          </Col>
+          )}
         </Row>
       </div>
     </>
