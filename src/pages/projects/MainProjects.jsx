d53c768c02c0806c81c46adb502c9b249a03936e
Yudi454 <lucasyudi445@gmail.com>
2025-06-06 14:23:39 -0300

    Se agregaron fotos a los proyectos

--- src/pages/projects/MainProjects.jsx
+++ src/pages/projects/MainProjects.jsx
@@ -12,10 +12,10 @@ const MainProjects = ({ color, proyectos }) => {
             key={proyecto.titulo}
           >
             <Card
-              style={{ width: "18rem" }}
+              style={{ width: "22rem" }}
               className={`fondoCard${color} texto${color}`}
             >
-              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Img variant="top" src={`${proyecto.imagen}`} className="img-fluid" />
               <Card.Body>
                 <Card.Title>{proyecto.titulo}</Card.Title>
                 <Card.Text>{proyecto.descripcion}</Card.Text>
@@ -23,13 +23,13 @@ const MainProjects = ({ color, proyectos }) => {
                   <Button
                     href={proyecto.linkFront}
                     target="_blank"
-                    className="me-4"
+                    className={`me-4 btn${color}`}
                   >
                     Ir al Front
                   </Button>
                 )}
                 {proyecto.linkBack && (
-                  <Button href={proyecto.linkBack} target="_blank">
+                  <Button href={proyecto.linkBack} target="_blank" className={`btn${color}`}>
                     Ir al Back
                   </Button>
                 )}
